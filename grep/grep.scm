81094e415502ceaa75239e714cfd0f01f00c1b1f
Łukasz Korecki <lukasz@coffeesounds.com>
2012-04-05 22:23:47 +0100

    move out functions to a separate file, unit test for reading files

--- grep/grep.scm
+++ grep/grep.scm
@@ -1,21 +1,10 @@
-(use srfi-1)
-(use regex)
+; This is the main "app" file, loads only needed functions
+(load "grep-lib.scm")
+; (for-each (lambda (l) (print l)) [argv])
 
-(define (get-pattern-and-file argument-list)
-  (list   (car(reverse argument-list)) (car(cdr (reverse argument-list)))))
+(define args (get-pattern-and-file [argv]))
+(define file-name (car args))
+(define pattern (car (reverse args)))
 
-(define (file-to-list file-name)
-  (string-split (read-file file-name) "\n"))
-
-(define (match-lines contents r-pattern)
-  (filter-map (lambda (line) (string-search r-pattern line)) contents))
-
-(for-each (lambda (l) (print l)) [argv])
-
-;
-;(define args (get-pattern-and-file [argv]))
-;(define file-name (car args))
-;(define pattern (car (reverse args)))
-;
-;(for-each (lambda (match) (print "> " match "\n"))
-;(match-lines (file-to-list file-name) pattern))
+(for-each (lambda (match) (print "> " match "\n"))
+          (match-lines (file-to-list file-name) pattern))
