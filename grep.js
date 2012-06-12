2ca964cc632902d59a61e67b164542a2fbdac164
Łukasz Korecki <lukasz@coffeesounds.com>
2012-06-12 23:02:25 +0100

    Almost evented grep ;-)

--- grep.js
+++ grep.js
@@ -1,7 +1,8 @@
 var u = require('util'),
     fs = require('fs'),
     ev = require('events');
-var processOptions = function(argv) {
+
+function processOptions(argv) {
   var opts = {};
   argv.forEach(function(value, index, arr){
     if(index == 2) opts.regex = new RegExp(value);
@@ -10,35 +11,28 @@ var processOptions = function(argv) {
 
   return opts;
 
-};
+}
 
-var grepFiles = function(files, regex, acceptor) {
+function grepFiles(files, regex, acceptor) {
+  function readAndMatch(file) {
+    fs.readFile(file, search);
+  }
 
-  var readAndMatch = function(file) {
-    fs.readFile(file, function(err,data){
-      if( err) return ;
-      data.toString().split("\n").forEach(function(line, index){
-        if(f = line.match(regex)) {
-          acceptor.emit('found', file, index+1, f.input);
-        }
-
-      });
-
+  function search(err,data){
+    if( err) return;
+    data.toString().split("\n").forEach(function(line, index){
+      if(line.match(regex)) acceptor.emit('found', file, index+1, line);
     });
+  }
 
-  };
   files.forEach(readAndMatch);
-};
+}
 
-options = processOptions(process.argv);
-
-console.log(u.inspect(options, false, 4, true));
-
-var watcher = new ev.EventEmitter();
+var options = processOptions(process.argv),
+    watcher = new ev.EventEmitter();
 
 watcher.on('found',function(file, line_no, line_str){
-
   console.log(file,line_no, line_str);
-
 });
+
 grepFiles(options.files, options.regex, watcher);
